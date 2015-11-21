b7f4919e988fdd151d5e7c0ac878eff919b180d6
Shreyans Sheth <dev.shreyans96@gmail.com>
2015-11-22 05:17:25 +0530

    Updated FloydWarshall

--- CODE/Coding Workspace/College stuff/CSE 220 (DSA)/FloydWarshall.cpp
+++ CODE/Coding Workspace/College stuff/CSE 220 (DSA)/FloydWarshall.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int dis[100][100];
-int adj_mat[100][100];
+int dis[251][251];
 
 void FloydWarshall(int n)
 {
 	for(int k=1;k<=n;k++)
 		for(int i=1;i<=n;i++)
-			for(int j=0;j<=n;j++)
-				if(dis[i][k]+dis[k][j] > dis[i][j])
+			for(int j=1;j<=n;j++)
+				if(i!=j && i!=k && j!=k && dis[i][k]+dis[k][j]<dis[i][j])
 					dis[i][j]=dis[i][k]+dis[k][j];
 }
 
 int main()
 {
 	int n;
-	cin>>n; //Enter number of vertices
-	for(int i=1;i<=n;i++)
+	cin>>n;
+	for(int i=1;i<=n;i++) //Input adjacency matrix
 		for(int j=1;j<=n;j++)
-		{
-			cin>>adj_mat[i][j];
-			dis[i][j]=dis;
-		}
+			cin>>dis[i][j];
 	FloydWarshall(n);
-	for(int i=1;i<=n;i++)
+	for(int i=1;i<=n;i++) //Printing shortest path from vertex i to j
 	{
 		for(int j=1;j<=n;j++)
 			cout<<dis[i][j]<<" ";
